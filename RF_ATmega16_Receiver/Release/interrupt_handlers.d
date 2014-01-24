d699940b486330a8103fbc6d18ae95441b0721b9
Tomasz Jokiel <tomasz.jokiel@gmail.com>
2014-01-24 22:11:23 +0100

    clean up mess on repo, update .gitignore

--- RF_ATmega16_Receiver/Release/interrupt_handlers.d
+++
@@ -1,8 +0,0 @@
-interrupt_handlers.d interrupt_handlers.o: ../interrupt_handlers.c \
-  ../headers/global_types.h ../lcd/lcd_jokii.h ../system/task.h
-
-../headers/global_types.h:
-
-../lcd/lcd_jokii.h:
-
-../system/task.h:
--- RF_ATmega16_Receiver/Release/util.d
+++
@@ -1,5 +0,0 @@
-util.d util.o: ../util.c ../headers/global_types.h ../headers/util.h
-
-../headers/global_types.h:
-
-../headers/util.h:
