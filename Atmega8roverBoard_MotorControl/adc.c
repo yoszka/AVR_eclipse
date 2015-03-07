e8c6ac78cb011ac0943869b45e8126b91131c876
Tomasz Jokiel <tomasz.jokiel@gmail.com>
2015-03-07 13:42:22 +0100

    ATmega8roverBoard_MotorControl changed ADC reference source to internal 2.56V

--- Atmega8roverBoard_MotorControl/adc.c
+++ Atmega8roverBoard_MotorControl/adc.c
@@ -12,7 +12,8 @@ void ADC_Init()
     ADCSRA = (1 << ADEN)  | (1 << ADIE)  |                 // enable ADC device; ADIE - enable interrupts
              (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);   // ADPS2:0 prescaler 128
 
-    ADMUX = (1 << ADLAR) | (1 << MUX2);                    // ADC lef adjust -> can use ADCH for 8-bit value, ADC4
+    ADMUX = (1 << ADLAR) | (1 << MUX2)                     // ADC lef adjust -> can use ADCH for 8-bit value, ADC4
+          | (1 << REFS0) | (1 << REFS1);                   // Internal reference voltage 2.56 V
 
     DDRC &= ~(1 << PC4);
 }
